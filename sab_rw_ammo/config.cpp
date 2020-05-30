82c103cd8654abcc31abbffdde6caad24f632913
Donovan Cunningham <donovancunningham@rocketmail.com>
2020-05-30 09:48:57 -0700

    Seperated Ammo into it's own file

---
+++ sab_rw_ammo/config.cpp
@@ -0,0 +1,112 @@
+class CfgPatches
+{
+    class sab_rw_ammo
+	{
+        author="Donov C.";
+		name="Secret Weapons Rework";
+        url="http://www.arma3.com";
+		version=1;
+		units[]=
+		{
+		};
+        weapons[]={};
+		requiredAddons[]=
+		{	
+			"sab_B_dumbbomb_base"
+			"sab_200Rnd_127x99_mag_Tracers_mag",
+			"sab_200Rnd_200x99_mag_Tracers_mag",
+			"sab_1rnd_50_mag",
+			"sab_680Rnd_20x80_mag",
+			"sab_3600Rnd_792_mag"
+		};
+    };
+};
+
+class cfgAmmo
+{
+	class sab_B_dumbbomb_base;
+    class sab_genericbomb_1000_ammo: sab_B_dumbbomb_base
+	{
+        hit=8000;
+		indirectHit=3000;
+		indirectHitRange=40;
+		suppressionRadiusHit=50;
+    };
+    class sab_genericbomb_500_ammo: sab_B_dumbbomb_base
+	{
+		hit=3000;
+		indirectHit=1000;
+		indirectHitRange=5;
+		explosive = 1;
+		suppressionRadiusHit=10;
+    };
+    class sab_genericbomb_250_ammo: sab_B_dumbbomb_base 
+	{
+        hit=1250;
+		explosive = 1;
+		indirectHit=425;
+		indirectHitRange=4;
+		suppressionRadiusHit=8;
+    };
+};
+class cfgMagazines
+{
+	class VehicleMagazine;
+    class sab_200Rnd_127x99_mag_Tracers_mag;
+	class sab_3600Rnd_792_mag;
+	class sab_1800Rnd_792_mag: sab_3600Rnd_792_mag
+	{
+		scope=2;
+		count=1800;
+		hit=100;
+	};
+    class sab_1800Rnd_127x99_mag_Tracers_mag: sab_200Rnd_127x99_mag_Tracers_mag
+	{
+		hit=180;
+		explosive = 0.2;
+		scope=2;
+		count=1800;
+		tracersEvery = 1;
+		tracerColorR = [0.1 , 1, 0, 1];
+	};
+    class sab_200Rnd_200x99_mag_Tracers_mag : VehicleMagazine
+	{
+        class Components {};
+		scope=2;
+		count=200;
+		hit = 520;
+        caliber = 20.0;
+		explosive = 0.4;
+		initSpeed=800;
+		maxLeadSpeed=200;
+		tracersEvery=1;
+		muzzleImpulseFactor[]={-0.0049999999,0.1};
+        aiAmmoUsageFlags="64 + 128 + 256";
+	};
+	class sab_680Rnd_20x80_mag: VehicleMagazine
+	{
+		scope=2;
+		count=680;
+		hit = 520;
+        caliber = 20.0;
+		explosive = 0.4;
+		initSpeed=800;
+		maxLeadSpeed=200;
+		tracersEvery=1;
+		muzzleImpulseFactor[]={-0.0049999999,0.1};
+		aiAmmoUsageFlags="64 + 128 + 256";
+	};
+	class sab_1rnd_50_mag;
+    class sab_1rnd_500_mag: sab_1rnd_50_mag
+	{
+		displayName="BMB 500 lb";
+		displayNameShort="500";
+		ammo="sab_genericbomb_500_ammo";
+		hardpoints[]=
+		{
+			"SAB_SW_BOMBS_B_H",
+			"SAB_SW_BOMBS_B"
+		};
+		count=1;
+	};
+};
